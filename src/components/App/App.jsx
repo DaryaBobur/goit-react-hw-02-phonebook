51124bd5821660d2700e2007ab017f457347e4f6
Darya <daryabobur@gmail.com>
2022-09-15 17:41:34 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,101 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from '../ContactsForm/ContactsForm';
+import ContactsList from '../ContactsList/ContactsList';
+import Filter from '../Filter/Filter';
+import { ContainerApp, Title, Subtitle } from './AppStyled';
+
+class App extends Component {
+  state = {
+    contacts: [   
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+    filter: '',
+  }
+
+  nameUserInputId = nanoid();
+
+  
+  addContact = (data) => {
+    if(this.duplicateName(data)) {
+      return alert(`${data.name} is already in contacts!`)
+    }
+
+    const contact = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    }
+    console.log(contact);
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }))
+  }
+
+  removeContact = (id) => {
+    this.setState((prevState) => {
+        const updateContacts = prevState.contacts.filter((contact) => contact.id !== id);
+
+        return {
+            contacts: updateContacts
+        }
+    })
+  }
+
+  filterNamesContacts = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+ 
+    if(!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLocaleLowerCase();
+    const filteredContacts = contacts.filter(({ name }) => {
+      const normalizedName = name.toLocaleLowerCase();
+      return normalizedName.includes(normalizedFilter);
+    })
+    return filteredContacts;
+    
+  };
+
+  duplicateName({name}) {
+    const { contacts } = this.state;
+    return contacts.find((contact) => contact.name === name);
+  }
+
+
+  
+
+ render() {
+  
+  return (
+<ContainerApp>
+  <Title>Phonebook</Title>
+  <ContactForm onSubmit={this.addContact}/>
+
+  <Subtitle>Contacts</Subtitle>
+
+  <Filter 
+   value={this.state.filter} 
+   onChange={this.filterNamesContacts}
+  />
+
+  <ContactsList 
+   contacts={this.getFilteredContacts()} 
+   removeContact={this.removeContact}
+  />
+
+</ContainerApp>
+  )
+};
+}
+
+
+
+export default App;
